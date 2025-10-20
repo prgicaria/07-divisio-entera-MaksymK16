8bbb75006ce88257ca609e5e8086ca754d994a32
MaksymK16 <mkutznietsov@instituticaia.cat>
2025-10-20 08:50:00 +0200

    correció 1

--- divisio.py
+++ divisio.py
@@ -2,6 +2,6 @@ a=int(input("Escriu el teu dividend"))
 b=float(input("Esciu el teu divisor"))
 quocient=a//b
 residu=a%b
-print ("Divisió:", a,"/",b)
+print (f"Divisió: {a}/{b}")
 print ("Quocient:", quocient)
 print ("Residu:", residu)
